e8b284825d1ef647dd12fed0d0f7ea9f1b8e094f
Excavator <kvronin@gmail.com>
2023-03-06 02:58:04 +0600

    add finctions for roles

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,12 +1,12 @@
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './users.models';
 import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/sequelize';
+import { InjectModel } from '@nestjs/sequelize'
 
 @Injectable()
 export class UsersService {
 
-  constructor(@InjectModel(User) private userRepository: typeof User) {}
+  constructor(@InjectModel(User)    private userRepository: typeof User) {}
 
   async createUser(dto: CreateUserDto) {
     const user = await this.userRepository.create(dto)
